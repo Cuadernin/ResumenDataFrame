651f7271e9e4ec565be9022d81444907d532222f
Erwin <75356078+Cuadernin@users.noreply.github.com>
2021-05-11 13:34:17 -0500

    Delete Resolucion.py

--- Resolucion.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from PyQt5 import QtCore
-import os
-
-os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
-qapp = QApplication(sys.argv)
-print(qapp.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling))
