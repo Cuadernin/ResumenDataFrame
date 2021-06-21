3fb70833a9f2af9ade092b0c0ae740e2b52b20b5
Erwin <75356078+Cuadernin@users.noreply.github.com>
2021-06-21 18:50:27 -0500

    LR with OLS

--- LinearRegression.py
+++ LinearRegression.py
@@ -12,7 +12,7 @@ def regresion(df,col,nombre):
     lista=list(df.dtypes=="object")
     nombres=[df.columns[i] for i in range(len(lista)) if lista[i]==True] #lista de columnas con valores de tipo string
     df=df.drop(nombres,axis="columns") #eliminamos estas columnas pues no las necesitamos
-    df=sm.add_constant(df) #agregamos una constante al modelo
+    df=sm.add_constant(df) #agregamos una constante al modelo para que sea del tipo y=a+bx+...+e
     reg=sm.OLS(df[col],df).fit() #llamamos al metodo OLS (metodo que hace la regresion) y ajustamos los daots
     with open(nombre,"a+") as f:
         f.write(str(reg.summary())) #escribimos el summary en un archivo de texto
